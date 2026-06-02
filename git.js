660101ed460c5c82f785b5a72fcc9307eef6d3d0
Lucasrsilva00 <lucassilvarodrigues483@gmail.com>
2026-06-02 12:13:30 -0300

    Update git.js
    Integrante B: Adicionado laço for para média térmica

--- git.js
+++ git.js
@@ -1,2 +1,6 @@
 function monitorarTemperatura(leituras) {
     let soma = 0;
+     for (let i = 0; i < leituras.length; i++) {
+        soma += leituras[i];
+    }
+    let media = soma / leituras.length;
