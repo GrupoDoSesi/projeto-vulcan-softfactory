52216d433d1eb164d840e0c0e54ae9144185ee8c
RyanErrice09 <ryan.rochacardoso@gmail.com>
2026-06-02 11:59:12 -0300

    Update motor.js
    Integrante A: Adicionado o controle de velocidade

--- motor.js
+++ motor.js
@@ -1,2 +1,8 @@
 function calcularVelocidadeFinal(velocidadeAtual, aceleracao, tempo = 2) {
     let velocidadeFinal = velocidadeAtual + (aceleracao * tempo);
+let status;
+    if (velocidadeFinal > 100) {
+        status = "ALERTA DE SOBRECARGA";
+    } else {
+        status = "Sistema Seguro";
+    }
