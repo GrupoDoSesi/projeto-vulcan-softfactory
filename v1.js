853640e1632671e578feadab9afc29968338051c
Sanders28 <sandersqueiroz63@gmail.com>
2026-06-02 12:16:47 -0300

    Update v1.js
    Integrante C: Configurando dados de teste do robô.

--- v1.js
+++ v1.js
@@ -1,2 +1,9 @@
 const calcularVelocidadeFinal = require('./motor');
 const monitorarTemperatura = require('./sensores');
+
+const velInicial = 30;
+const aceleracao = 40;
+const temperaturas = [42, 44, 46, 43, 49]; 
+
+const dadosMotor = calcularVelocidadeFinal(velInicial, aceleracao, 2);
+const dadosSensores = monitorarTemperatura(temperaturas);
