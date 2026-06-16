9c48affdc61e2ce9b1813203494faa118e27e943
Sanders28 <sandersqueiroz63@gmail.com>
2026-06-16 11:19:57 -0300

    Update v1.js
    Integrante C: Painel integrado com as 5 funções do grupo

--- v1.js
+++ v1.js
@@ -1,19 +1,29 @@
 const calcularVelocidadeFinal = require('./motor');
 const monitorarTemperatura = require('./sensores');
+const emitirAvisoPainel = require('./alertas');      
+const validarDadosRobo = require('./validacoes');    
 
 const velInicial = 30;
 const aceleracao = 40;
 const temperaturas = [42, 44, 46, 43, 49]; 
 
-const dadosMotor = calcularVelocidadeFinal(velInicial, aceleracao, 2);
-const dadosSensores = monitorarTemperatura(temperaturas);
+const checagem = validarDadosRobo(velInicial, aceleracao);
 
-console.log("=========================================");
-console.log("       PAINEL DE TELEMETRIA VULCAN       ");
-console.log("=========================================");
-console.log(`STATUS DO MOTOR:     [ ${dadosMotor.status} ]`);
-console.log(`VELOCIDADE FINAL:    ${dadosMotor.velocidadeFinal} m/s`);
-console.log("-----------------------------------------");
-console.log(`MÉDIA TÉRMICA:       ${dadosSensores.media}°C`);
-console.log(`LIGAR RESFRIAMENTO?  [ ${dadosSensores.precisaResfriamento ? "SIM" : "NÃO"} ]`);
-console.log("=========================================");
+if (!checagem.dadosValidos) {
+    console.log(checagem.mensagemValidacao);
+} else {
+    const dadosMotor = calcularVelocidadeFinal(velInicial, aceleracao, 2);
+    const dadosSensores = monitorarTemperatura(temperaturas);
+
+    const avisosSistema = emitirAvisoPainel(dadosMotor.status, dadosSensores.precisaResfriamento);
+
+    console.log("=========================================");
+    console.log("       PAINEL DE TELEMETRIA VULCAN       ");
+    console.log("=========================================");
+    console.log(`VELOCIDADE FINAL:    ${dadosMotor.velocidadeFinal} m/s`);
+    console.log(`MÉDIA TÉRMICA:       ${dadosSensores.media}°C`);
+    console.log("-----------------------------------------");
+    console.log("STATUS E ALERTAS DO SISTEMA:");
+    console.log(avisosSistema);
+    console.log("=========================================");
+}
